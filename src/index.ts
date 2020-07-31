c093927d9a72fd4c77583c1327617fb1e57e720e
mostafa.drz <mostafa69d@gmail.com>
2020-07-31 14:44:53 -0400

    just some more tooling

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,20 @@
-import AWS from "aws-sdk";
-const comprehand = new AWS.Comprehend({
-  apiVersion: "2017-11-27",
-  region: "ca-central-1",
+import AWS, { Comprehend } from 'aws-sdk';
+const comprehand = new Comprehend({
+  apiVersion: '2017-11-27',
+  region: 'ca-central-1'
 });
-
 const isItPositive = async (title: string) => {
   const params = {
-    LanguageCode: "en",
-    TextList: [title],
+    LanguageCode: 'en',
+    TextList: [title]
   };
 
   comprehand.batchDetectSentiment(params, (error, data) => {
     if (error) {
-      console.error("something is not happy", error);
+      console.error('something is not happy', error);
     } else {
-      console.log("All I know is", data);
+      console.log('All I know is', data);
       console.log(data.ResultList[0].SentimentScore);
     }
   });
 };
-
-function main() {
-  const text = process.argv[2];
-  isItPositive(text);
-}
-
-main();
