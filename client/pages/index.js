fa38150da41f9ab2719e7e210f2f67a852510e23
mostafa.drz <mostafa69d@gmail.com>
2020-08-06 21:16:48 -0400

    add query param to be able to filter negative and neutral as well

--- client/pages/index.js
+++ client/pages/index.js
@@ -23,8 +23,20 @@ export default function Home(props) {
 }
 
 export async function getServerSideProps(context) {
+  const {
+    query: { sentiment },
+  } = context;
+  const sentiments = {
+    positive: "POSITIVE",
+    negative: "NEGATIVE",
+    neutral: "NEUTRAL",
+  };
+  let sen = "POSITIVE";
+  if (sentiments[sentiment]) {
+    sen = sentiments[sentiment];
+  }
   const resp = await fetch(
-    `${process.env.API_URL}/api/results?Sentiment=POSITIVE`
+    `${process.env.API_URL}/api/results?Sentiment=${sen}`
   );
   const data = await resp.json();
   return {
